bd654dde11d834a15df4a3b8a570b4e88a54cd39
Thad Duval <witherberry@thads-mbp.attlocal.net>
2016-12-29 13:51:47 -0500

    Basic sign in layout complete

--- DevslopesSocial/SignInVC.swift
+++ DevslopesSocial/SignInVC.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class SignInVC: UIViewController {
 
+    
+    @IBOutlet weak var headerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        headerImageView.image = UIImage(named: "BONERNEWSNETWORKLOGO")
+        headerImageView.backgroundColor = UIColor.white
+        headerImageView.layer.cornerRadius = 65.0
+        headerImageView.clipsToBounds = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
