1dacabdfdbe364035d7ef714befd34f13c54c6a6
Thad Duval <witherberry@thads-mbp.attlocal.net>
2016-12-30 15:44:40 -0500

    Completed creating firebase db users

--- DevslopesSocial/SignInVC.swift
+++ DevslopesSocial/SignInVC.swift
@@ -57,7 +57,8 @@ class SignInVC: UIViewController {
             } else  {
                 print("TADTADTAD: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -71,7 +72,8 @@ class SignInVC: UIViewController {
                         print("TADTADTAD: User authenticated with Firebase")
                         
                         if let user = user {
-                            self.completeSignIn(id: user.uid)
+                            let userData = ["provider": user.providerID]
+                            self.completeSignIn(id: user.uid, userData: userData)
                         }
                         
                     } else {
@@ -82,9 +84,9 @@ class SignInVC: UIViewController {
                                 print("TADTADTAD: Successfully authenticated with Firebase")
                                 
                                 if let user = user {
-                                    self.completeSignIn(id: user.uid)
+                                    let userData = ["provider": user.providerID]
+                                    self.completeSignIn(id: user.uid, userData: userData)
                                 }
-                                
                             }
                         })
                     }
@@ -92,8 +94,10 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String,String>) {
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
+        
+        DataService.ds.createFirebaseDBUser(uid: id,userData: userData)
         
         print("TADTADTAD: Data saved to Keychain \(keychainResult)")
         
